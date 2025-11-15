ac43d032aa1cb3fb0f8e575bf8e704aa75fa15ea
MichaelSong <slp7756646@126.com>
2025-11-15 18:01:18 +0800

    refactor(chat): standardize code formatting and improve readability
    - Changed single quotes to double quotes for consistency
    - Enhanced code formatting for better readability
    - Updated type annotations and spacing for clarity

--- app/components/chat/index.tsx
+++ app/components/chat/index.tsx
@@ -16,7 +16,10 @@ import ChatImageUploader from '@/app/components/base/image-uploader/chat-image-u
 import ImageList from '@/app/components/base/image-uploader/image-list'
 import { useImageFiles } from '@/app/components/base/image-uploader/hooks'
 import FileUploaderInAttachmentWrapper from '@/app/components/base/file-uploader-in-attachment'
-import type { FileEntity, FileUpload } from '@/app/components/base/file-uploader-in-attachment/types'
+import type {
+  FileEntity,
+  FileUpload,
+} from '@/app/components/base/file-uploader-in-attachment/types'
 import { getProcessedFiles } from '@/app/components/base/file-uploader-in-attachment/utils'
 
 export interface IChatProps {
@@ -45,7 +48,7 @@ const Chat: FC<IChatProps> = ({
   isHideSendInput = false,
   onFeedback,
   checkCanSend,
-  onSend = () => { },
+  onSend = () => {},
   useCurrentUserAvatar,
   isResponding,
   controlClearQuery,
@@ -94,34 +97,55 @@ const Chat: FC<IChatProps> = ({
     onClear,
   } = useImageFiles()
 
-  const [attachmentFiles, setAttachmentFiles] = React.useState<FileEntity[]>([])
+  const [attachmentFiles, setAttachmentFiles] = React.useState<FileEntity[]>(
+    [],
+  )
 
   const handleSend = () => {
-    if (!valid() || (checkCanSend && !checkCanSend())) { return }
-    const imageFiles: VisionFile[] = files.filter(file => file.progress !== -1).map(fileItem => ({
-      type: 'image',
-      transfer_method: fileItem.type,
-      url: fileItem.url,
-      upload_file_id: fileItem.fileId,
-    }))
+    if (!valid() || (checkCanSend && !checkCanSend())) {
+      return
+    }
+    const imageFiles: VisionFile[] = files
+      .filter(file => file.progress !== -1)
+      .map(fileItem => ({
+        type: 'image',
+        transfer_method: fileItem.type,
+        url: fileItem.url,
+        upload_file_id: fileItem.fileId,
+      }))
     const docAndOtherFiles: VisionFile[] = getProcessedFiles(attachmentFiles)
     const combinedFiles: VisionFile[] = [...imageFiles, ...docAndOtherFiles]
     onSend(queryRef.current, combinedFiles)
-    if (!files.find(item => item.type === TransferMethod.local_file && !item.fileId)) {
-      if (files.length) { onClear() }
+    if (
+      !files.find(
+        item => item.type === TransferMethod.local_file && !item.fileId,
+      )
+    ) {
+      if (files.length) {
+        onClear()
+      }
       if (!isResponding) {
         setQuery('')
         queryRef.current = ''
       }
     }
-    if (!attachmentFiles.find(item => item.transferMethod === TransferMethod.local_file && !item.uploadedId)) { setAttachmentFiles([]) }
+    if (
+      !attachmentFiles.find(
+        item =>
+          item.transferMethod === TransferMethod.local_file && !item.uploadedId,
+      )
+    ) {
+      setAttachmentFiles([])
+    }
   }
 
   const handleKeyUp = (e: any) => {
     if (e.code === 'Enter') {
       e.preventDefault()
       // prevent send message when using input method enter
-      if (!e.shiftKey && !isUseInputMethod.current) { handleSend() }
+      if (!e.shiftKey && !isUseInputMethod.current) {
+        handleSend()
+      }
     }
   }
 
@@ -148,14 +172,16 @@ const Chat: FC<IChatProps> = ({
         {chatList.map((item) => {
           if (item.isAnswer) {
             const isLast = item.id === chatList[chatList.length - 1].id
-            return <Answer
-              key={item.id}
-              item={item}
-              feedbackDisabled={feedbackDisabled}
-              onFeedback={onFeedback}
-              isResponding={isResponding && isLast}
-              suggestionClick={suggestionClick}
-            />
+            return (
+              <Answer
+                key={item.id}
+                item={item}
+                feedbackDisabled={feedbackDisabled}
+                onFeedback={onFeedback}
+                isResponding={isResponding && isLast}
+                suggestionClick={suggestionClick}
+              />
+            )
           }
           return (
             <Question
@@ -163,78 +189,85 @@ const Chat: FC<IChatProps> = ({
               id={item.id}
               content={item.content}
               useCurrentUserAvatar={useCurrentUserAvatar}
-              imgSrcs={(item.message_files && item.message_files?.length > 0) ? item.message_files.map(item => item.url) : []}
+              imgSrcs={
+                item.message_files && item.message_files?.length > 0
+                  ? item.message_files.map(item => item.url)
+                  : []
+              }
             />
           )
         })}
       </div>
-      {
-        !isHideSendInput && (
-          <div className='fixed z-10 bottom-0 left-1/2 transform -translate-x-1/2 pc:ml-[122px] tablet:ml-[96px] mobile:ml-0 pc:w-[794px] tablet:w-[794px] max-w-full mobile:w-full px-3.5'>
-            <div className='p-[5.5px] max-h-[150px] bg-white border-[1.5px] border-gray-200 rounded-xl overflow-y-auto'>
-              {
-                visionConfig?.enabled && (
-                  <>
-                    <div className='absolute bottom-2 left-2 flex items-center'>
-                      <ChatImageUploader
-                        settings={visionConfig}
-                        onUpload={onUpload}
-                        disabled={files.length >= visionConfig.number_limits}
-                      />
-                      <div className='mx-1 w-[1px] h-4 bg-black/5' />
-                    </div>
-                    <div className='pl-[52px]'>
-                      <ImageList
-                        list={files}
-                        onRemove={onRemove}
-                        onReUpload={onReUpload}
-                        onImageLinkLoadSuccess={onImageLinkLoadSuccess}
-                        onImageLinkLoadError={onImageLinkLoadError}
-                      />
-                    </div>
-                  </>
-                )
-              }
-              {
-                fileConfig?.enabled && (
-                  <div className={`${visionConfig?.enabled ? 'pl-[52px]' : ''} mb-1`}>
-                    <FileUploaderInAttachmentWrapper
-                      fileConfig={fileConfig}
-                      value={attachmentFiles}
-                      onChange={setAttachmentFiles}
-                    />
-                  </div>
-                )
-              }
-              <Textarea
-                className={`
+      {!isHideSendInput && (
+        <div className="fixed z-10 bottom-0 left-1/2 transform -translate-x-1/2 pc:ml-[105px] tablet:ml-[76px] mobile:ml-0 pc:w-[68%] tablet:w-[72%] mobile:tablet:w-full max-w-full mobile:w-[95%] px-3.5">
+          <div className="p-[5.5px] max-h-[150px] bg-white border-[1.5px] border-gray-200 rounded-xl overflow-y-auto">
+            {visionConfig?.enabled && (
+              <>
+                <div className="absolute bottom-2 left-2 flex items-center">
+                  <ChatImageUploader
+                    settings={visionConfig}
+                    onUpload={onUpload}
+                    disabled={files.length >= visionConfig.number_limits}
+                  />
+                  <div className="mx-1 w-[1px] h-4 bg-black/5" />
+                </div>
+                <div className="pl-[52px]">
+                  <ImageList
+                    list={files}
+                    onRemove={onRemove}
+                    onReUpload={onReUpload}
+                    onImageLinkLoadSuccess={onImageLinkLoadSuccess}
+                    onImageLinkLoadError={onImageLinkLoadError}
+                  />
+                </div>
+              </>
+            )}
+            {fileConfig?.enabled && (
+              <div
+                className={`${visionConfig?.enabled ? 'pl-[52px]' : ''} mb-1`}
+              >
+                <FileUploaderInAttachmentWrapper
+                  fileConfig={fileConfig}
+                  value={attachmentFiles}
+                  onChange={setAttachmentFiles}
+                />
+              </div>
+            )}
+            <Textarea
+              className={`
                   block w-full px-2 pr-[118px] py-[7px] leading-5 max-h-none text-base text-gray-700 outline-none appearance-none resize-none
                   ${visionConfig?.enabled && 'pl-12'}
                 `}
-                value={query}
-                onChange={handleContentChange}
-                onKeyUp={handleKeyUp}
-                onKeyDown={handleKeyDown}
-                autoSize
-              />
-              <div className="absolute bottom-2 right-6 flex items-center h-8">
-                <div className={`${s.count} mr-3 h-5 leading-5 text-sm bg-gray-50 text-gray-500 px-2 rounded`}>{query.trim().length}</div>
-                <Tooltip
-                  selector='send-tip'
-                  htmlContent={
-                    <div>
-                      <div>{t('common.operation.send')} Enter</div>
-                      <div>{t('common.operation.lineBreak')} Shift Enter</div>
-                    </div>
-                  }
-                >
-                  <div className={`${s.sendBtn} w-8 h-8 cursor-pointer rounded-md`} onClick={handleSend}></div>
-                </Tooltip>
+              value={query}
+              onChange={handleContentChange}
+              onKeyUp={handleKeyUp}
+              onKeyDown={handleKeyDown}
+              autoSize
+            />
+            <div className="absolute bottom-2 right-6 flex items-center h-8">
+              <div
+                className={`${s.count} mr-3 h-5 leading-5 text-sm bg-gray-50 text-gray-500 px-2 rounded`}
+              >
+                {query.trim().length}
               </div>
+              <Tooltip
+                selector="send-tip"
+                htmlContent={
+                  <div>
+                    <div>{t('common.operation.send')} Enter</div>
+                    <div>{t('common.operation.lineBreak')} Shift Enter</div>
+                  </div>
+                }
+              >
+                <div
+                  className={`${s.sendBtn} w-8 h-8 cursor-pointer rounded-md`}
+                  onClick={handleSend}
+                ></div>
+              </Tooltip>
             </div>
           </div>
-        )
-      }
+        </div>
+      )}
     </div>
   )
 }
